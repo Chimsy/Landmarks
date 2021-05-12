9583db91aa20a93f892b93d1dd5ef265a9a475d8
Golden Chimusinde <Golden.Chimusinde@cassavasmartech.co.zw>
2021-05-12 10:47:43 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Golden Chimusinde on 12/5/2021.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
