2be5958cf130bdf2cdcc3c02ba1004f55e03feae
Golden Tapfuma Chimusinde <goldenchimsy@gmail.com>
2021-05-19 20:42:47 +0200

    House Clean Up

---
+++ Landmarks/View/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Golden Chimusinde on 12/5/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+        }
+    }
+}
